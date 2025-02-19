3defcb85c806a7bb619fd534afd6130ed940027e
Terence Schumacher <terenceschumacher@gmail.com>
2025-02-18 23:31:54 -0800

    updated status

--- README.md
+++ README.md
@@ -5,5 +5,12 @@
 - [ ] Component inventory
 - [ ] timesheets OFM
 - [ ] documentation for ramp up and reference
+- [ ] Retrieve App Image from S3
+- [ ] Investigate bootstrapping the converter into the running program
+
+
+## Gotchas (Blockers)
+- Conversion of dwg to dxf
+- 
 
 
--- src/main/kotlin/com/tazzledazzle/draftflow/DraftFlowApplication.kt
+++ src/main/kotlin/com/tazzledazzle/draftflow/DraftFlowApplication.kt
@@ -2,10 +2,16 @@ package com.tazzledazzle.draftflow
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.io.File
 
 @SpringBootApplication
 class DraftFlowApplication
 
 fun main(args: Array<String>) {
-    runApplication<DraftFlowApplication>(*args)
+//    runApplication<DraftFlowApplication>(*args)
+    val converterFileName = "ODAFileConverter_QT6_lnxX64_8.3dll_25.12.AppImage"
+    val dwgFile = "src/test/resources/2023-800 Columbia (6105)-XREF-Base Details.dwg"
+    File("src/main/resources/").walk().forEach {
+        ProcessBuilder("$it/$converterFileName", dwgFile).start().waitFor()
+    }
 }
