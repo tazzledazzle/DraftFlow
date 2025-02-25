b5f69937f345e6de7c4684a9f88085d4bdc2997d
Terence Schumacher <terenceschumacher@gmail.com>
2025-02-25 14:27:39 -0800

    defined Task

--- src/main/kotlin/models/Task.kt
+++ src/main/kotlin/models/Task.kt
@@ -1,4 +1,10 @@
 package models
 
-class Task {
-}
+data class Task (
+    var id: String,
+    var projectId: String,
+    var name: String,
+    var description: String,
+    var estimatedHours: Int,
+    var createdAt: String
+)
