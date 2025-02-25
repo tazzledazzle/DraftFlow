dc0d2ccfa115eb366380d806d04d89d2a83109d0
Terence Schumacher <terenceschumacher@gmail.com>
2025-02-25 14:25:59 -0800

    added more skeleton and defined Project.kt

--- src/main/kotlin/models/Project.kt
+++ src/main/kotlin/models/Project.kt
@@ -1,4 +1,11 @@
 package models
 
-class Project {
-}
+data class Project (
+    var id: String,
+    var projectManagerId: String,
+    var name: String,
+    var description: String,
+    var startDate: String,
+    var endDate: String,
+    var createdAt: String
+)
