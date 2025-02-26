4baebcb67ccdcbce99364ed560c58e62d3246bac
Terence Schumacher <terenceschumacher@gmail.com>
2025-02-26 11:14:13 -0800

    return a user

--- src/main/kotlin/services/UserService.kt
+++ src/main/kotlin/services/UserService.kt
@@ -4,9 +4,16 @@ import models.User
 import models.UserRole
 
 class UserService {
-    fun createUser(firstName: String, lastName: String = "", email: String = "", password: String, userRole: UserRole): User {
+    fun createUser(firstName: String, lastName: String = "", email: String = "", password: String = "", userRole: UserRole = UserRole.FOREMAN, createdAt: String = ""): User {
         println("User created: $firstName")
 
-        return User(firstName = firstName)
+        return User(
+            firstName = firstName,
+            lastName = lastName,
+            email = email,
+            password = password,
+            role = userRole,
+            createdAt = createdAt
+        )
     }
 }
