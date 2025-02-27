48fe67eec772ad1f184412915660836daf9b8dd2
Terence Schumacher <terenceschumacher@gmail.com>
2025-02-27 11:26:24 -0800

    defined entity relations for task

--- src/main/kotlin/models/Task.kt
+++ src/main/kotlin/models/Task.kt
@@ -1,10 +1,27 @@
 package models
 
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "tasks")
 data class Task (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
+
+    @Column(nullable = false)
     var projectId: Long = 0L,
+    @Column
     var name: String = "",
+    @Column
     var description: String = "",
+    @Column()
     var estimatedHours: Double = 0.0,
+    @Column(name = "created_at", nullable = false, updatable = false)
     var createdAt: String = ""
 )
