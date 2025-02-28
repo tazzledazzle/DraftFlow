ecfcdf6906d0ced3e56e1060e129a08768a18b5e
Terence Schumacher <terenceschumacher@gmail.com>
2025-02-28 13:11:50 -0800

    setup schema

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,5 +1,19 @@
+import java.sql.Connection
+import java.sql.DriverManager
+
+
 class App {
     fun main() {
-        println("Hello, world!")
+        val url = "jdbc:postgresql://your-database-url:5432/projects"
+        val username = "tschumacher"
+        val password = "password"
+
+        try {
+            val connection: Connection? = DriverManager.getConnection(url, username, password)
+            var stmt = connection?.createStatement()!!
+            var rs = stmt.executeQuery("SELECT * FROM projects")
+        } catch (e: Exception) {
+            TODO("Not yet implemented")
+        }
     }
 }
