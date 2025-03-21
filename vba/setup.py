aa9dfeed4c276d5c4e141d04a190b366bc3e0ecf
Terence Schumacher <terenceschumacher@gmail.com>
2025-03-20 19:11:45 -0700

    changed sheet name; updated requirements; tested xlwing; generated custom test framework

--- vba/setup.py
+++ vba/setup.py
@@ -1,2 +1,17 @@
 import pandas as pd
-df = pd.read_excel("")
+import datetime as dt
+import xlwings as xw
+import numpy as np
+
+
+random = pd.DataFrame(data=np.random.randn(100, 5), columns=[f"Trial {i}" for i in range(1, 6)])
+# df = pd.read_excel("tests/Test-Lists-Stephanie 5.21.24 version.xlsm")
+print(random)
+
+# xw.view(random)
+
+vba_book = xw.Book("tests/TestXLWingsSheet.xlsm")
+
+test_wing = vba_book.macro("Module8.TestXlWing")
+
+print(test_wing())
