141d0fe6200abdaad3f94a89a049938d8280cf69
Terence Schumacher <terenceschumacher@gmail.com>
2025-03-21 15:11:02 -0700

    python version

--- vba/src/excel_grouping_test.py
+++ vba/src/excel_grouping_test.py
@@ -10,34 +10,48 @@ class GroupingTest(ExcelVBATestCase):
         # Test grouping functionality
 
         book = xlwings.Book("../tests/Test6446Job.xlsx")
-        out_book = xlwings.Book("../tests/TestXLWingsSheet.xlsm")
-        job_track_sheet = out_book.sheets("Job Cost Tracking")
         sheet = book.sheets("Sheet1")
         task_groups = {}
+        # since this is being generated from test data might be a later todo
+        project_id = sheet.range("F2").value
 
         # loop through the cells in the range B2:B1500
         # L is task Id that needs to be generated from projectId and taskId
-        for cell, hrs, est in zip(
-            sheet.range("B2:B1500"), sheet.range("C2:C1500"), sheet.range("K2:K1500")
-        ):
+        num_task_id = 1
+        for cell, hrs, est in zip(sheet.range("B2:B1500"), sheet.range("C2:C1500"), sheet.range("K2:K1500")):
+            # sheet.range(f"L{cell.row}").value = f"{project_id}-{num_task_id}"
+            num_task_id += 1
+            print(cell.value)
             if cell.value is not None:
-                print(cell.value)
                 if cell.value not in task_groups:
-                    task_groups[cell.value] = hrs.value
+                    task_groups[cell.value] = TaskInfo(task_id=f"{project_id}-{num_task_id}",
+                                                   task_name=cell.value,
+                                                   task_hours=hrs.value if hrs.value is not None else 0.0,
+                                                   task_estimate=est.value if est.value is not None else 0.0
+                                                   )
                 else:
-                    task_groups[cell.value] += (
+                    task_groups[cell.value].task_hours += (
                         hrs.value if hrs.value is not None else 0.0
+                    )
+                    task_groups[cell.value].task_estimate += (
+                        est.value if est.value is not None else 0.0
                     )
 
         print(task_groups)
-        new_sheet = book.sheets.add("Task Groups")
-        book.save()
-        new_sheet.range("A1").value = "Task Group"
-        new_sheet.range("B1").value = "Hours"
+        out_book = xlwings.Book("../tests/TestXLWingsSheet.xlsm")
+        job_track_sheet = out_book.sheets("Job Cost Tracking")
+        # new_sheet = book.sheets.add("Task Groups")
+        # book.save()
+        # new_sheet.range("A1").value = "Task Group"
+        # new_sheet.range("B1").value = "Hours"
+        # new_sheet.range("C1").value = "Estimate"
+        # new_sheet.range("D1").value = "TaskId"
+
         i = 2
-        for key, value in zip(list(task_groups.keys()), list(task_groups.values())):
-            new_sheet.range(f"A{i}").value = key
-            new_sheet.range(f"B{i}").value = value
+        for key, task in task_groups.items():
+            job_track_sheet.range(f"A{i}").value = task.task_name
+            job_track_sheet.range(f"C{i}").value = task.task_estimate
+            job_track_sheet.range(f"D{i}").value = task.task_hours
             i += 1
 
         book.save()
@@ -45,3 +59,11 @@ class GroupingTest(ExcelVBATestCase):
         book.close()
 
         pass
+
+
+class TaskInfo:
+    def __init__(self, task_id, task_name, task_hours, task_estimate):
+        self.task_id = task_id
+        self.task_name = task_name
+        self.task_hours = task_hours
+        self.task_estimate = task_estimate
