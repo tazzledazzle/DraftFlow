f7bfb542cf2317984ec3adeb2862e4bba525e4d2
Terence Schumacher <terenceschumacher@gmail.com>
2025-03-28 19:49:27 -0700

    F** added userService impl

--- timesheet-manager/src/main/kotlin/com/northshore/timesheet/service/impl/UserService.kt
+++ timesheet-manager/src/main/kotlin/com/northshore/timesheet/service/impl/UserService.kt
@@ -1,7 +1,10 @@
 package com.northshore.timesheet.service.impl
 
 import com.northshore.timesheet.entity.User
+import com.northshore.timesheet.repository.UserRepository
 import org.apache.el.stream.Optional
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
 
 
 interface UserService {
@@ -9,4 +12,29 @@ interface UserService {
     fun findById(id: Long): Optional<User>
     fun getAllUsers(): List<User>
     fun createUser(user: User): User
+}
+
+@Service
+class UserServiceImpl(
+    private val userRepository: UserRepository,
+    private val passwordEncoder: PasswordEncoder
+) : UserService {
+
+    override fun findByUsername(username: String): Optional<User> {
+        return userRepository.findByUsername(username)
+    }
+
+    override fun findById(id: Long): Optional<User> {
+        return userRepository.findById(id)
+    }
+
+    override fun getAllUsers(): List<User> {
+        return userRepository.findAll()
+    }
+
+    override fun createUser(user: User): User {
+        // Encode password before saving
+        user.password = passwordEncoder.encode(user.password)
+        return userRepository.save(user)
+    }
 }
